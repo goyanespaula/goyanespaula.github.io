9289bb5c8f38163a1350c79a794a5e92c252bcf3
Paula Goyanes <goyanespaula@gmail.com>
2018-05-25 14:53:05 -0700

    more mobile friendly navbar

--- scripts/script.js
+++ scripts/script.js
@@ -12,18 +12,10 @@ typeIt("Paula Goyanes", ".typing");
 $(document).ready(function() {
   var $main = $("#main");
   var mainbottom = $main.offset().top + $main.height();
-  $(window).on("scroll", function() {
-    // we round here to reduce a little workload
-    var stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
-      $("nav").addClass("past-main");
-    } else {
-      $("nav").removeClass("past-main");
-    }
-  });
-});
+  var $navBar = $(".nav-bar");
+  var $navHamburger = $(".nav-hamburger");
+  var $navDropDown = $(".nav-dropdown");
 
-$(document).ready(function() {
   // Add smooth scrolling to all links
   $("a").on("click", function(event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -46,6 +38,28 @@ $(document).ready(function() {
           window.location.hash = hash;
         }
       );
+    }
+  });
+
+  $(window).on("scroll", function() {
+    // we round here to reduce a little workload
+    var stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+      $navBar.addClass("past-main");
+      $navDropDown.addClass("past-main");
+    } else {
+      $navBar.removeClass("past-main");
+      $navDropDown.removeClass("past-main");
+    }
+  });
+
+  $navHamburger.on("click", function() {
+    $navDropDown.toggleClass("show");
+  });
+
+  $(window).resize(function() {
+    if ($(document).width() > 565) {
+      $navDropDown.removeClass("show");
     }
   });
 });
